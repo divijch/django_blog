ffaa11cbbba84d876552d78e82ac31fbe7a641cc
Divij Chaudhry <divijchaudhry@MacBook-Pro.local>
2024-06-13 13:05:59 +0530

    posts by category template inherite context processors

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from blogs.models import Blog
+
 
 def home(request):
-    return render(request, 'home.html')
+
+    featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('-created_at')
+    posts = Blog.objects.filter(is_featured=False, status="Published").order_by('-created_at')
+    context = {
+
+        'featured_posts': featured_posts,
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+
+from blogs.models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    # Fetch the posts the belongs to the category with the category_id
+    posts = Blog.objects.filter(status="Published", category=category_id)
+   # try:
+       #category = Category.objects.get(pk=category_id)
+    #except:
+       # return redirect('home')
+    #use try and except to redrirect to home or do something or use get object or 404 for 404 page
+    
+    category = get_object_or_404(Category,  pk=category_id) 
+    
+    context = {
+        'posts': posts,
+        'category': category,
+    } 
+    return render(request, 'posts_by_category.html', context)

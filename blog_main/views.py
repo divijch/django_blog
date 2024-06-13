836c32d12bbb95945240abf9ef521002cf4fb450
Divij Chaudhry <divijchaudhry@MacBook-Pro.local>
2024-06-13 17:54:05 +0530

    register login logout feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . forms import RegistrationForm
 from blogs.models import Blog
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
 
@@ -13,3 +14,37 @@ def home(request):
         'posts': posts
     }
     return render(request, 'home.html', context)
+
+def register(request):
+    if request.method == 'POST':   
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else: 
+        form = RegistrationForm
+    context = {
+        'form': form,
+    }
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                redirect('home') 
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')

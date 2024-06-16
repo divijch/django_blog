7124afc972b948da6d9c07c1e9e9e1b753ff20c2
Divij Chaudhry <divijchaudhry@MacBook-Pro.local>
2024-06-16 21:11:11 +0530

    category crud feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,6 +3,9 @@ from . forms import RegistrationForm
 from blogs.models import Blog
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
 
@@ -26,11 +29,13 @@ def register(request):
     context = {
         'form': form,
     }
+    
     return render(request, 'register.html', context)
+
 
 def login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
@@ -38,8 +43,10 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                redirect('home') 
-    form = AuthenticationForm()
+                return redirect('dashboard')  # Use return to perform the redirection after login
+    else:
+        form = AuthenticationForm()
+    
     context = {
         'form': form,
     }
